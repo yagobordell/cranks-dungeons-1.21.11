b57214d2536e1e3b26b525de5470cc3cebdee858
Yago Bordell <yagobordell@gmail.com>
2026-01-15 20:15:29 +0100

    Initial commit

---
+++ src/main/java/com/cranks/dungeons/CranksDungeonsClient.java
@@ -0,0 +1,10 @@
+package com.cranks.dungeons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CranksDungeonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/cranks/dungeons/CranksDungeonsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cranks.dungeons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CranksDungeonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
