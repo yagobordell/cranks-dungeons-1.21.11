891c96a8078e3ad48e324c7cab8499d16953aa69
Yago Bordell <yagobordell@gmail.com>
2026-01-28 02:11:22 +0100

    Auto Attack Fix

--- src/main/java/com/cranks/dungeons/mixin/ClientAttackMixin.java
+++ src/main/java/com/cranks/dungeons/mixin/ClientAttackMixin.java
@@ -39,29 +39,34 @@ public abstract class ClientAttackMixin {
         this.attackCooldown = 0;
 
         if (options.attackKey.isPressed()) {
+            // Check if we're targeting an entity every tick (not just on initial press)
             boolean isTargetingEntity = crosshairTarget != null &&
                     crosshairTarget.getType() == HitResult.Type.ENTITY;
 
-            if (isTargetingEntity) {
-                float progress = player.getAttackCooldownProgress(0.0F);
+            float progress = player.getAttackCooldownProgress(0.0F);
 
-                if (progress < 1.0F) {
-                    cranks$wasNotReady = true;
-                    cranks$isReadyToSnap = false;
-                } else if (progress >= 1.0F && cranks$wasNotReady) {
-                    if (cranks$isReadyToSnap) {
+            // Track when cooldown drops below 100%
+            if (progress < 1.0F) {
+                cranks$wasNotReady = true;
+                cranks$isReadyToSnap = false;
+            }
+            // When cooldown reaches 100% and we previously saw it wasn't ready
+            else if (progress >= 1.0F && cranks$wasNotReady) {
+                if (cranks$isReadyToSnap) {
+                    // Only attack if we're currently targeting an entity
+                    if (isTargetingEntity) {
                         this.doAttack();
                         cranks$isReadyToSnap = false;
                         cranks$wasNotReady = false;
-                    } else {
-                        cranks$isReadyToSnap = true;
                     }
+                    // If not targeting, keep the ready state so we attack when we do target
+                } else {
+                    // Become ready to attack on next tick (if targeting entity)
+                    cranks$isReadyToSnap = true;
                 }
-            } else {
-                cranks$isReadyToSnap = false;
-                cranks$wasNotReady = false;
             }
         } else {
+            // Reset when attack key is released
             cranks$isReadyToSnap = false;
             cranks$wasNotReady = false;
         }
